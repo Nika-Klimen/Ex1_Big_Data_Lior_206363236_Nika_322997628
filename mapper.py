6874df8a1cfc2c498f659fd9831d54ce600a8318
Nika Klimenchuk <141430580+Nika-Klimen@users.noreply.github.com>
2025-11-27 15:02:03 +0200

    Create mapper.py

---
+++ mapper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+
+def read_input(file):
+    for line in file:
+        # split the line into words
+        yield line.split()
+
+def main(separator='\t'):
+    # input comes from STDIN (standard input)
+    data = read_input(sys.stdin)
+    for words in data:
+        for word in words:
+            word = word.strip()
+            word = word.rstrip('.,')
+            word = word.lower()
+            if word:
+                print('%s%s%d' % (word, separator, 1))
+
+if __name__ == "__main__":
+    main()
